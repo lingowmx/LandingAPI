fb701e643bfd0a7f6f5c65e469e30ccf7c4e79a6
lingowmx <clingow@hotmail.com>
2023-12-15 13:36:25 +0100

    imagenes y ajustes antes de icono de hamburguesa

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,3 +1,31 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const menuLinks = document.querySelectorAll('nav ul li a');
+    menuLinks.forEach(link => {
+        link.addEventListener('click', scrollToSection);
+    });
+    function scrollToSection(event){
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if(targetSection){
+            targetSection.scrollIntoView({behavior: 'smooth'});
+        }
+    }
+});
+
+let scrolltoTop = document.getElementById("toTopButton");
+window.addEventListener("scroll", function(){
+    if(document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
+        scrolltoTop.style.display = "block";
+    } else{
+        scrolltoTop.style.display = "none";
+    }
+});
+scrolltoTop.addEventListener("click", function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0
+})
+
 const API =
   "https://spotify23.p.rapidapi.com/artist_albums/?id=0L8ExT028jH3ddEcZwqJJ5&offset=1&limit=12";
 const content = null || document.getElementById('content')
@@ -27,13 +55,13 @@ async function fetchData(urlApi) {
           <img src="${album.releases.items[0].coverArt.sources[0].url}" alt="${album.releases.items[0].name}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-black">
+          <h2 class="text-lg text-black mx-auto">
             <span aria-hidden="true" class="absolute inset-0"></span>
             ${album.releases.items[0].name}
-          </h3>
+          </h2>
         </div>
       </div>
-    `)}
+    `) .slice(0, 8).join("")}
       `;
       content.innerHTML = view;
   } catch (error) {
